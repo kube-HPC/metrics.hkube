fa9c93bfaded9a1661fa1049c38631efbe2a158f
yehiyam <yehiyam@gmail.com>
2017-12-05 17:21:40 +0200

    add internal and external services

---
+++ test/expressTest.js
@@ -0,0 +1,154 @@
+const EventEmitter = require('events').EventEmitter;
+const metrics = require('../index');
+const expect = require('chai').expect
+const client = require('prom-client');
+const RestServer = require('@hkube/rest-server');
+const http_mocks = require('node-mocks-http');
+const get_port=require('get-port')
+const config = {
+    metrics: {
+
+    }
+}
+
+let server;
+describe('With RestServer or internal server', () => {
+    beforeEach(() => {
+        client.register.clear();
+        
+    });
+    
+    it('should init without server',async () => {
+        await metrics.init({
+        });
+    });
+
+    describe('with RestServer', () => {
+        beforeEach(() => {
+            server = new RestServer();
+        });
+        afterEach(async () => {
+            if (server){
+                await server.stop();
+            }
+    
+        });
+        it('should init with express', async () => {
+           
+            await metrics.init(config.metrics);
+            const port = await get_port();
+            const {app} = await server.start({
+                routes:[metrics.getRouter()],
+                port
+            })
+            const measure = metrics.addTimeMeasure({
+                name:'m1',
+                labels:['l1','l2']
+            })
+            const id = measure.start({labelValues:{
+                l1:1,
+                l2:2
+            }});
+            measure.end({id});
+            const mock_response = http_mocks.createResponse();
+            const mock_request = http_mocks.createRequest({
+                method: 'GET',
+                url: '/metrics'
+            });
+            app.handle(mock_request, mock_response);
+            expect(mock_response.statusCode).to.eq(200);
+            expect(mock_response._getData()).to.include('m1_counter{l1="1",l2="2"} 1');
+            expect(mock_response._getData()).to.include('m1_histogram_count{l1="1",l2="2"} 1');
+
+        });
+        it('should init with express custom path', async () => {
+            await metrics.init({
+                server: {
+                    path:'/myMetrics'
+                }
+            });
+            const port = await get_port();
+            const {app} = await server.start({
+                routes:[metrics.getRouter()],
+                port
+            })
+            const measure = metrics.addTimeMeasure({
+                name:'m1',
+                labels:['l1','l2']
+            })
+            const id = measure.start({labelValues:{
+                l1:1,
+                l2:2
+            }});
+            measure.end({id});
+            const mock_response = http_mocks.createResponse();
+            const mock_request = http_mocks.createRequest({
+                method: 'GET',
+                url: '/myMetrics'
+            });
+            app.handle(mock_request, mock_response);
+            expect(mock_response.statusCode).to.eq(200);
+            expect(mock_response._getData()).to.include('m1_counter{l1="1",l2="2"} 1');
+            expect(mock_response._getData()).to.include('m1_histogram_count{l1="1",l2="2"} 1');
+
+        });
+    })
+
+    describe('with internal server', () => {
+        it('should create server', async () => {
+            const port = await get_port();
+            await metrics.init({
+                server: {
+                    port
+                }
+            });
+            const app = metrics._server._app;
+            const measure = metrics.addTimeMeasure({
+                name:'m1',
+                labels:['l1','l2']
+            })
+            const id = measure.start({labelValues:{
+                l1:1,
+                l2:2
+            }});
+            measure.end({id});
+            const mock_response = http_mocks.createResponse();
+            const mock_request = http_mocks.createRequest({
+                method: 'GET',
+                url: '/metrics'
+            });
+            app.handle(mock_request, mock_response);
+            expect(mock_response.statusCode).to.eq(200);
+            expect(mock_response._getData()).to.include('m1_counter{l1="1",l2="2"} 1');
+            expect(mock_response._getData()).to.include('m1_histogram_count{l1="1",l2="2"} 1');
+        });
+        it('should create server with custom path', async () => {
+            const port = await get_port();
+            await metrics.init({
+                server: {
+                    port,
+                    path:'/myMetrics'
+                }
+            });
+            const app = metrics._server._app;
+            const measure = metrics.addTimeMeasure({
+                name:'m1',
+                labels:['l1','l2']
+            })
+            const id = measure.start({labelValues:{
+                l1:1,
+                l2:2
+            }});
+            measure.end({id});
+            const mock_response = http_mocks.createResponse();
+            const mock_request = http_mocks.createRequest({
+                method: 'GET',
+                url: '/myMetrics'
+            });
+            app.handle(mock_request, mock_response);
+            expect(mock_response.statusCode).to.eq(200);
+            expect(mock_response._getData()).to.include('m1_counter{l1="1",l2="2"} 1');
+            expect(mock_response._getData()).to.include('m1_histogram_count{l1="1",l2="2"} 1');
+        });
+    });
+});
--- test/test.js
+++ test/test.js
@@ -1,10 +1,10 @@
-const Metrics = require('../index');
+const metrics = require('../index');
 const expect = require('chai').expect
 const client = require('prom-client');
 
 const config={
     metrics:{
-        collectDefault:true
+        collectDefault:true,
     }
 }
 
@@ -12,21 +12,22 @@ describe('Init metrics', () => {
     beforeEach(() => {
         client.register.clear();
     });
-    it('Should read config', () => {
-        const metrics = new Metrics(config.metrics);
+    it('Should read config',async () => {
+        // const metrics = new Metrics(config.metrics);
+        await metrics.init(config.metrics)
         expect(metrics._options.collectDefault).to.be.true 
     });
-    it('Should init without options', () => {
-        const metrics = new Metrics();
+    it('Should init without options',async () => {
+        await metrics.init()
         expect(metrics._options.collectDefault).to.be.false 
         
     });
-    it('Should throw if no name in option', () => {
-        const metrics = new Metrics();
+    it('Should throw if no name in option',async () => {
+        await metrics.init()
         expect(()=>metrics.addTimeMeasure({})).to.throw(Error)
     });
-    it('Should add and remove time measure', () => {
-        const metrics = new Metrics(config.metrics);
+    it('Should add and remove time measure',async () => {
+        await metrics.init(config.metrics)
         expect(metrics._metrics.size).to.eq(0);
         metrics.addTimeMeasure({
             name:'m1',
@@ -36,8 +37,8 @@ describe('Init metrics', () => {
         metrics.removeMeasure('m1');
         expect(metrics._metrics.size).to.eq(0);
     });
-    it('Should return measure', () => {
-        const metrics = new Metrics(config.metrics);
+    it('Should return measure',async () => {
+        await metrics.init(config.metrics)
         const measure = metrics.addTimeMeasure({
             name:'m1',
             labels:['l1','l2']
@@ -45,10 +46,10 @@ describe('Init metrics', () => {
         expect(measure).to.exist;
         expect(measure._histogram).to.exist;
         expect(measure._requestCounter).to.exist;
-        expect(measure._startTime).to.be.null;
+        expect(measure._startedMeasures).to.be.empty;
     });
-    it('should get measure by name', () => {
-        const metrics = new Metrics(config.metrics);
+    it('should get measure by name',async () => {
+        await metrics.init(config.metrics)
         const measure = metrics.addTimeMeasure({
             name:'m1',
             labels:['l1','l2']
@@ -56,31 +57,31 @@ describe('Init metrics', () => {
         const getMeasure = metrics.get('m1');
         expect(getMeasure).to.eql(measure);         
     });
-    it('should throw if no name for get measure by name', () => {
-        const metrics = new Metrics(config.metrics);
+    it('should throw if no name for get measure by name',async () => {
+        await metrics.init(config.metrics);
         expect(()=>metrics.get()).to.throw(Error)
     });
-    it('should return null if name does not exist', () => {
-        const metrics = new Metrics(config.metrics);
+    it('should return null if name does not exist',async () => {
+        await metrics.init(config.metrics);
         const getMeasure = metrics.get('m1');
         expect(getMeasure).to.not.exist;        
     });
-    it('Should start measure without id', () => {
-        const metrics = new Metrics(config.metrics);
+    it('Should start measure without id', async() => {
+        await metrics.init(config.metrics);
         const measure = metrics.addTimeMeasure({
             name:'m1',
             labels:['l1','l2']
         })
         const id = measure.start();
-        expect(measure._startTime).to.exist;
+        expect(measure._startedMeasures).to.include.keys(id);
         expect(id).to.exist;
         measure.end({id});
-        expect(measure._startTime).to.not.exist;
+        expect(measure._startedMeasures).to.not.include.keys(id);;
         
     });
 
-    it('Should copy labels', () => {
-        const metrics = new Metrics(config.metrics);
+    it('Should copy labels', async() => {
+        await metrics.init(config.metrics);
         const measure = metrics.addTimeMeasure({
             name:'m1',
             labels:['l1','l2']
@@ -88,15 +89,15 @@ describe('Init metrics', () => {
         const id = measure.start({labelValues:{
             l1:1
         }});
-        expect(measure._labelValues).to.include({l1:1})
+        expect(measure._startedMeasures.get(id).labels).to.include({l1:1})
         measure.end({id,labelValues:{l2:2}});
-        expect(measure._labelValues).to.include({l2:2,l1:1})
+        // expect(measure._startedMeasures.get(id).labels).to.include({l2:2,l1:1})
         expect(client.register.metrics()).to.include('m1_counter{l1="1",l2="2"} 1');
         expect(client.register.metrics()).to.include('m1_histogram_count{l1="1",l2="2"} 1');
     });
 
-    it('Should override labels', () => {
-        const metrics = new Metrics(config.metrics);
+    it('Should override labels',async () => {
+        await metrics.init(config.metrics);
         const measure = metrics.addTimeMeasure({
             name:'m1',
             labels:['l1','l2']
@@ -105,21 +106,22 @@ describe('Init metrics', () => {
             l1:1,
             l2:2
         }});
-        expect(measure._labelValues).to.include({l1:1})
+        expect(measure._startedMeasures.get(id).labels).to.include({l1:1})
         measure.end({id,labelValues:{l2:3}});
-        expect(measure._labelValues).to.include({l2:3,l1:1})
+        // expect(measure._labelValues).to.include({l2:3,l1:1})
+        expect(client.register.metrics()).to.include('m1_counter{l1="1",l2="3"} 1');
         
     });
-    it('Should work without labels', () => {
-        const metrics = new Metrics(config.metrics);
+    it('Should work without labels',async () => {
+        await metrics.init(config.metrics);
         const measure = metrics.addTimeMeasure({
             name:'m1',
             labels:['l1','l2']
         })
         const id = measure.start();
-        expect(measure._labelValues).to.be.empty
+        expect(measure._startedMeasures.get(id).labels).to.be.empty
         measure.end({id});
-        expect(measure._labelValues).to.be.empty
+        expect(measure._startedMeasures).to.be.empty
         expect(client.register.metrics()).to.include('m1_counter 1');
         expect(client.register.metrics()).to.include('m1_histogram_count 1');
     });
